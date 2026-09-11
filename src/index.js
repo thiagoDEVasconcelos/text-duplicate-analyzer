c9c998d82a3e61d248fb2ed0165fcb1c5ee79b74
Thiago Sousa <thiagovsousa21@gmail.com>
2026-09-11 12:50:07 -0300

    feat: Criando funções para fazer a filtragem das palavras presentes no texto.

--- src/index.js
+++ src/index.js
@@ -4,17 +4,28 @@ const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
 
 fs.readFile(link, 'utf8', (err, data) => {
-  verificaPalavrasDuplicadas(data);
+  quebraEmParagrafos(data);
 });
 
+function quebraEmParagrafos (texto) {
+  const listaParagrafos = texto.toLowerCase().split("\n");
+  verificaPalavrasDuplicadas(listaParagrafos);
+}
 
-function verificaPalavrasDuplicadas(texto) {
-  const listaPalavras = texto.split(" ");
+function limpaPalavras(listaDePalavras) {
+  return listaDePalavras.map((palavra) => {
+    return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()']/g, '');
+  });
+}
+
+function verificaPalavrasDuplicadas(paragrafos) {
+  const listaPalavras = paragrafos.join(" ").split(" ");
+  const palavrasLimpas = limpaPalavras(listaPalavras).filter((palavra) => palavra !== "");
   const resultado = {};
 
-  listaPalavras.forEach((palavra) => {
+  palavrasLimpas.forEach((palavra) => {
     resultado[palavra] = (resultado[palavra] || 0) + 1;
   })
 
-  console.log(resultado);
+  console.log("RESULTADO", resultado);
 }
