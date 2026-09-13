2797020c8e0aafa2fd4dcb9e09843749cfa1492f
Thiago Sousa <thiagovsousa21@gmail.com>
2026-09-13 12:51:57 -0300

    feat: Create a function to save the created file.

--- src/cli.js
+++ src/cli.js
@@ -1,15 +1,28 @@
 import fs from 'fs';
-import { trataErros } from './errors/funcoesErro.js';
+import trataErros from './errors/funcoesErro.js';
 import { quebraEmParagrafos } from './index.js';
 
 const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
+const endereco = caminhoArquivo[3];
 
 fs.readFile(link, 'utf8', (err, data) => {
   try{
     if(err) throw err;
-    quebraEmParagrafos(data);
+    const resultado = quebraEmParagrafos(data);
+    criaESalvaArquivo(resultado, endereco);
   } catch(err) {
     trataErros(err);
   }
 });
+
+async function criaESalvaArquivo (listaPalavras, enderecoArquivo) {
+  const caminhoArquivo = `${enderecoArquivo}/resultado.txt`;
+  const textoPalavras = JSON.stringify(listaPalavras);
+  try {
+    await fs.promises.writeFile(caminhoArquivo, textoPalavras);
+    console.log("Arquivo criado com sucesso");
+  } catch (err) {
+    throw err;
+  }
+}
