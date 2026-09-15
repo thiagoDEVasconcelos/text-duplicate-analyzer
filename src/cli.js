c89a2b138da94d78de8eb4040df1e777da37e2c1
Thiago Sousa <thiagovsousa21@gmail.com>
2026-09-14 22:07:27 -0300

    refactor: Organizando o código de cli.js e usando as bibliotecas Commander e Chalk para melhorar a visualização na linha de comando.

--- src/cli.js
+++ src/cli.js
@@ -1,28 +1,60 @@
 import fs from 'fs';
+import path from 'path';
 import trataErros from './errors/funcoesErro.js';
-import { quebraEmParagrafos } from './index.js';
+import chalk from 'chalk';
+import { contaPalavras } from './index.js';
+import { montaSaidaArquivo } from './helpers.js';
+import { Command } from 'commander';
 
-const caminhoArquivo = process.argv;
-const link = caminhoArquivo[2];
-const endereco = caminhoArquivo[3];
+const program = new Command();
 
-fs.readFile(link, 'utf8', (err, data) => {
-  try{
-    if(err) throw err;
-    const resultado = quebraEmParagrafos(data);
-    criaESalvaArquivo(resultado, endereco);
-  } catch(err) {
-    trataErros(err);
-  }
+program
+  .version('0.0.1')
+  .option('-t, --texto <string>', 'caminho do texto a ser processado')
+  .option('-d, --destino <string>', 'caminho da pasta onde salvar o arquivo de resultados')
+  .action((options) => {
+    const { texto, destino } = options;
+    console.log('CHEGUEI AQUI');
+    
+    if (!texto || !destino) {
+      console.error(chalk.red('erro: favor inserir caminho de origem e destino'));
+      program.help();
+      return;
+    }
+    
+    const caminhoTexto = path.resolve(texto);
+    const caminhoDestino = path.resolve(destino);
+
+    try {
+      processaArquivo(caminhoTexto, caminhoDestino);
+      console.log(chalk.green('Processamento concluído com sucesso!'));
+    } catch(erro) {
+      console.log(chalk.red('Ocorreu um erro ao processar o arquivo:', erro.message));
+      ;
+    }
 });
 
-async function criaESalvaArquivo (listaPalavras, enderecoArquivo) {
-  const caminhoArquivo = `${enderecoArquivo}/resultado.txt`;
-  const textoPalavras = JSON.stringify(listaPalavras);
+function processaArquivo(texto, destino) {
+  fs.readFile(texto, 'utf-8', (erro, texto) => {
+    try {
+      if (erro) throw erro
+      const resultado = contaPalavras(texto);
+      criaESalvaArquivo(resultado, destino)
+    } catch(erro) {
+      trataErros(erro);
+    }
+  })
+}
+
+async function criaESalvaArquivo(listaPalavras, endereco) {
+  const arquivoNovo = `${endereco}/resultado.txt`;
+  const textoPalavras = montaSaidaArquivo(listaPalavras);
   try {
-    await fs.promises.writeFile(caminhoArquivo, textoPalavras);
-    console.log("Arquivo criado com sucesso");
-  } catch (err) {
-    throw err;
+    await fs.promises.writeFile(arquivoNovo, textoPalavras);
+    console.log('arquivo criado');
+  } catch(erro) {
+    throw erro;
   }
 }
+
+program.parse(process.argv);
